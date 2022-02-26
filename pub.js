7622b0fdaf7309251a24ad28bdb343841155bc4e
mrrtmob <mrrtmob@gmail.com>
2022-02-26 14:13:14 +0700

    first commit

---
+++ pub.js
@@ -0,0 +1,12 @@
+// MQTT publisher
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://localhost:1883')
+var topic = 'Tmob'
+var message = '1'
+
+client.on('connect', ()=>{
+    setInterval(()=>{
+        client.publish(topic, message)
+        console.log('Message sent!', message)
+    }, 5000)
+})
