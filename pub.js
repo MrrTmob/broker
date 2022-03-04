ec7b6a1d1208f9a0224622be93d6fe0a37a29583
Tmob <mrrtmob@gmail.com>
2022-03-05 05:21:46 +0700

    cert

--- pub.js
+++ pub.js
@@ -1,6 +1,6 @@
 // MQTT publisher
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtt://localhost',{port:8883})
+var client = mqtt.connect('mqtts://localhost',{port:8443})
 // console.log(client)
 var topic = 'Tmob'
 var message = '1'
