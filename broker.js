e49f8b9127604707c9d96aa23c23a5926a4e0014
Tmob <mrrtmob@gmail.com>
2022-03-05 05:28:23 +0700

    cert

--- broker.js
+++ broker.js
@@ -8,7 +8,7 @@ var settings = {port: 1883,
       level: 40,
     },
     secure : {
-      port: 8883,
+      port: 8443,
       keyPath: SECURE_KEY,
       certPath: SECURE_CERT,
     }
--- pub.js
+++ pub.js
@@ -1,6 +1,6 @@
 // MQTT publisher
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtts://mosquitto.d2a.link',{port:8883})
+var client = mqtt.connect('mqtts://mosquitto.d2a.link',{port:8443})
 // console.log(client)
 var topic = 'Tmob'
 var message = '1'
