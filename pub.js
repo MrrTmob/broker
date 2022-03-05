db963b6fca71268e17f8f2b87d564711968968cd
Tmob <mrrtmob@gmail.com>
2022-03-05 20:38:40 +0700

    cert

--- pub.js
+++ pub.js
@@ -1,6 +1,6 @@
 // MQTT publisher
 var mqtt = require('mqtt')
-var client = mqtt.connect('mqtts://mosquitto.d2a.link:8443')
+var client = mqtt.connect('mqtt://tmob.d2a.link:1883')
 //console.log(client)
 var topic = 'Tmob'
 var message = '1'
